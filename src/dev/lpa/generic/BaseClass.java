94169d9f661146ca27a5ad5b32aeea9b87c93979
adamd <adam.d.hinton@gmail.com>
2024-03-28 00:58:07 -0400

    flesh out BaseClass

--- src/dev/lpa/generic/BaseClass.java
+++ src/dev/lpa/generic/BaseClass.java
@@ -1,4 +1,20 @@
 package dev.lpa.generic;
 
+// Extensible but not customizable
 public class BaseClass {
+
+    public void recommendedMethod(){
+        System.out.println("[BaseClass.recommendedMethod]: Best way to do it");
+
+//        optionalMethod();
+//        mandatoryMethod();
+    }
+
+    protected void optionalMethod(){
+        System.out.println("BaseClass.optional: Customize optional method");
+    }
+
+    private void mandatoryMethod(){}
+    System.out.println("BaseClass.mandatoryMethod: NON-NEGOTIABLE")
+
 }
